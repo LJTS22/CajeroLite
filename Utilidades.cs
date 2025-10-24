c86936e6fe81e3f1b2fbd7a45cdb44c6707de79b
SharonC23 <carrillosharon263@gmail.com>
2025-10-23 21:10:09 -0500

    validacion de utilidades "Terminado"

--- Utilidades.cs
+++ Utilidades.cs
@@ -1,12 +1,34 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace CajeroLite
+namespace CajeroApp
 {
-    internal class Utilidades
+    class Program
     {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("=== Cajero Automático ===");
+            Console.WriteLine("1. Consultar saldo");
+            Console.WriteLine("2. Retirar dinero");
+            Console.WriteLine("3. Salir");
+            Console.Write("Seleccione una opción: ");
+            int opcion = int.Parse(Console.ReadLine());
+
+            if (UtilidadesCajero.ValidarOpcion(opcion, 1, 3))
+            {
+                Console.WriteLine("Opción válida.");
+
+                if (opcion == 2)
+                {
+                    Console.Write("Ingrese el monto a retirar: ");
+                    decimal monto = decimal.Parse(Console.ReadLine());
+                    if (UtilidadesCajero.ValidarMonto(monto))
+                    {
+                        Console.WriteLine($"Se retirarán ${monto}");
+                    }
+                }
+            }
+
+            UtilidadesCajero.Pausar();
+        }
     }
 }
